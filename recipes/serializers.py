e6f0d397f7651a493abc5d038a59d8f1d1fa44aa
Jonatan Engstad <jonaengs@gmail.com>
2020-04-04 11:50:00 +0200

    add ingredient and recipeiningredient serializers. Add id and default servings to recipeserializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
-from .models import Recipe
+from .models import Recipe, Ingredient, RecipeIngredient
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Recipe
-        fields = ('name', 'date_created', 'author')
+        fields = ('name', 'date_created', 'author', 'id', 'default_servings', )
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ('name', 'id', 'ubiquitous', )
+
+
+class RecipeIngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecipeIngredient
+        fields = ('id', 'measurement', 'amount_per_serving', )
